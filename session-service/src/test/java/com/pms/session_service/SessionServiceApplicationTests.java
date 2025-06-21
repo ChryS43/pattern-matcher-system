b9a0fe9a4597bc8971a96fcc0c5d1e915aabd554
christindenisi <chry3425@gmail.com>
2025-06-21 23:51:46 +0200

    feat(cloud-readiness): Session service + Eureka

--- session-service/src/test/java/com/pms/session_service/SessionServiceApplicationTests.java
+++ session-service/src/test/java/com/pms/session_service/SessionServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.pms.session_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SessionServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
