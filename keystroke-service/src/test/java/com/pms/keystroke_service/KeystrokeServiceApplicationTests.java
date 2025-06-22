667cf5207e9f08567cc7d1693f87e6be15cb9765
christindenisi <chry3425@gmail.com>
2025-06-23 00:45:31 +0200

    feat(cloud-readiness): rabbitmq and keystroke-service

--- keystroke-service/src/test/java/com/pms/keystroke_service/KeystrokeServiceApplicationTests.java
+++ keystroke-service/src/test/java/com/pms/keystroke_service/KeystrokeServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.pms.keystroke_service;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KeystrokeServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
